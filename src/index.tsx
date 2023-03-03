0b1e0413642de4f8b0a4d31bd09bfbaefbe62b04
Matias Cipollone <107125293+MatiasCipollone@users.noreply.github.com>
2023-03-03 17:34:13 -0300

    porfolio-personal

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import "regenerator-runtime/runtime";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import store from "./src/store";
-import App from "./src/App";
+import store from "./store";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
